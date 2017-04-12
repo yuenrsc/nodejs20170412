9e9249a6fe8f13919464a279f5d1d9d04886d6fb
Roddy <yuenrsc@gmail.com>
2017-04-12 10:23:38 +0000

    date parse done

--- server.js
+++ server.js
@@ -1,10 +1,27 @@
 var express = require('express')
-var app = express()
+var app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.get('/:date', function(req, res){
+    
+    var data = req.params.date;
+    
+    var date;
+    
+    if (/^[0-9]+$/g.test(data)){
+        date = new Date(parseInt(data));
+        var date_string = date.toLocaleString('en-US', {
+            "month" : "long",
+            "day" : "numeric",
+            "year" : "numeric"
+        });
+        res.send({"unix": data, "natural": date_string});
+    }else if (!isNaN(date = Date.parse(data))){
+        res.json({"unix":date, "natural":data});
+    }else{
+        res.json({"unix":null, "natural":null});
+    }
+    
+    //res.send(data);
+});
 
-app.listen(8080, function () {
-  console.log('Example app listening on port 8080!')
-})
+app.listen(8080);
